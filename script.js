d844b38ac43a5744bf4be52f1e3a6ecc41877c77
Adarsh4775 <137145969+Adarsh4775@users.noreply.github.com>
2023-10-08 18:10:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your code here
+const enteredText = document.getElementById("Evtext");
+const letterCount = document.getElementById("lettertext");
+
+enteredText.addEventListener("input",() => {
+	let text = enteredText.value;
+	let count =text.length;
+
+	letterCount.textContent=count;
+});
